8c1ceea2b8ce1fd7f001c8e23a4148c0c366aae5
chrnk-exe <ivan_kot2001@mail.ru>
2022-11-28 19:31:53 +0300

    timer

--- src/client/src/components/Phone/Finish.tsx
+++ src/client/src/components/Phone/Finish.tsx
@@ -1,5 +1,6 @@
-import React, { type FC } from 'react';
-import { Box, ClickAwayListener, Typography } from '@mui/material';
+import React, { type FC, useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
+// import ClickAwayListener from '@mui/material/ClickAwayListener';
 import { useTranslation } from 'react-i18next';
 
 type Props = {
@@ -9,20 +10,43 @@ type Props = {
 
 const Finish: FC<Props> = ({ handleCallOn, nextStep }) => {
 	const { t } = useTranslation();
+	const [timer, setTime] = useState(5);
 
-	const onClickAway = () => {
-		nextStep();
-		handleCallOn();
-	};
+	// const onClickAway = () => {
+	// 	nextStep();
+	// 	handleCallOn();
+	// };
+
+	useEffect(() => {
+		let timeleft = timer;
+		const MyTimer = setInterval(() => {
+			timeleft -= 1;
+			if (timeleft === 0) {
+				clearInterval(MyTimer);
+				nextStep();
+				handleCallOn();
+			}
+			setTime(timeleft);
+		}, 1000);
+		return () => clearInterval(MyTimer);
+	}, []);
 
 	return (
-		<ClickAwayListener onClickAway={onClickAway}>
-			<Box onClick={onClickAway}>
-				<Typography variant="h4" sx={{ color: '#FFFFF1' }}>
-					{t('talk_ended')}
-				</Typography>
-			</Box>
-		</ClickAwayListener>
+		// <ClickAwayListener onClickAway={onClickAway}>
+		<Box
+			display="flex"
+			flexDirection="column"
+			justifyContent="center"
+			alignItems="center"
+			gap={2}>
+			<Typography variant="h4" sx={{ color: '#FFFFF1' }}>
+				{t('talk_ended')}
+			</Typography>
+			<Typography variant="h4" sx={{ color: '#FFFFF1' }}>
+				{t('next_call') + timer.toString()}
+			</Typography>
+		</Box>
+		// </ClickAwayListener>
 	);
 };
 
